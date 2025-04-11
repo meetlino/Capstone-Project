3cf31b7a1f8e4ae5002bc4ff60634fd761a0c195
meetlino <lino2695@gmail.com>
2025-04-11 08:36:17 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+function Header(){
+    return (
+    <>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+    </>
+    );
+}
+
+export default Header;
